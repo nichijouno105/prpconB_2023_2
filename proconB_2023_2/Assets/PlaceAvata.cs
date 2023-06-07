ac855229ec7ab6b0025bd29954784035bf14ce1c
nodoka_hi <sakusakukazu@outlook.jp>
2023-06-07 16:24:14 +0900

    ボタンとアクションの対応を追加しました。

--- proconB_2023_2/Assets/PlaceAvata.cs
+++ proconB_2023_2/Assets/PlaceAvata.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class PlaceAvata : MonoBehaviour
 {
     public GameObject avatar;
+    // public GameObject PlayerPrefab;
+    // public GameObject PlayerInstance;
     //Dungeon script;
     void Start()
     {
     //script = GameObject.Find("Dungeon").GetComponent<Dungeon>();
     GameObject avatarObj = Instantiate(avatar, new Vector3(Dungeon.startPos.x, -0.5f, Dungeon.startPos.y), Quaternion.identity) as GameObject;
     avatarObj.transform.parent = transform;
+
+    // PlayerInstance = Instantiate(PlayerPrefab, transform.position, transform.rotation);
+    // PlayerAction playerAction = PlayerInstance.GetComponent<PlayerAction>();
+
+    // // handColliderを直接設定する
+    // playerAction.handCollider = GetComponentInChildren<SphereCollider>();
+    
     }
 }
